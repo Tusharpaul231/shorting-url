8d31a82e21e7a1c9cf01eac5bea346f1687826bb
Tushar Paul <56092836+Tusharpaul231@users.noreply.github.com>
2025-03-07 16:40:19 +0530

    Create url.js

---
+++ models/url.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema(
+    {
+        visitorId: {
+            type : String,
+            required : true,
+            unique : true,
+        },
+        redirectedURL: {
+            type : String,
+            required : true,
+        },
+        visitHistory: [{
+            timestamp: {
+                type : Number,
+            }
+        }],
+    },
+    { timestamps : true }
+);
+
+const URL = mongoose.model("url", urlSchema)
+
+module.exports = URL;
